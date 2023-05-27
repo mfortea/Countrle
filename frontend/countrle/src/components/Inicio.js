f837654fe7c8bbf01299797bd8747e9f763e84fa
Mateo Fortea Dugo <mfortea@Air-de-Mateo.home>
2023-05-27 17:03:08 +0200

    Nueva pantalla de Inicio

--- frontend/countrle/src/components/Inicio.js
+++ frontend/countrle/src/components/Inicio.js
@@ -1,34 +1,78 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import './Inicio.css'; 
-
+import './Inicio.css';
+import iphone_mockup from '../assets/iphone_mockup.png';
+import captura_pwa from '../assets/captura_pwa.png';
+import trofeo from '../assets/trofeo.png';
 
 const Inicio = () => {
-
   return (
     <div>
       <br></br>
       <div class="container animacion-carga">
         <div class="jumbotron">
-        <div id="mundo"></div>
-
+          <div id="mundo"></div>
           <h1>¡Bienvenido a Countrle!</h1>
           <br></br>
-          <p class="lead">Countrle es una emocionante aplicación en la que podrás poner a prueba tus conocimientos sobre países y adivinar palabras relacionadas con ellos. ¿Estás listo para desafiar tu mente y ampliar tus horizontes geográficos?
+          <p class="lead">Countrle es una emocionante juego en la que podrás poner a prueba tus conocimientos sobre países y adivinar palabras relacionadas con ellos. ¿Estás listo para desafiar tu mente y ampliar tus horizontes geográficos? </p>
+          <a className="btn btn-large btn-primary" href="/login">¡Jugar Ahora!</a>
+          <br></br>
+          <br></br>
+          <br></br>
+          <div class="container marketing">
 
-            El objetivo de Countrle es descubrir la palabra relacionada con un país, donde cada letra acertada te acerca más a la respuesta correcta. Pon a prueba tu habilidad para descifrar palabras y demuestra cuánto sabes sobre geografía.
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">Amplía tus horizontes geográficos <span class="text-muted">con Countrle.</span></h2>
+                <p class="lead">
+                  En cada ronda, te presentaremos una palabra encriptada y tendrás que poner a prueba tu conocimiento y agudeza para descifrarla. Cada letra que aciertes te acercará más a la respuesta correcta y, en el proceso, aprenderás más sobre la geografía, la cultura y las tradiciones del país en cuestión
+                  Con Countrle, te enfrentarás a retos cada vez más difíciles, pero el aprendizaje y la diversión están garantizados. No solo tendrás la oportunidad de demostrar cuánto sabes sobre geografía, sino que también podrás expandir tus horizontes y descubrir nuevos lugares y culturas que quizá no conocías.
+                </p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" src={iphone_mockup} alt="Captura de pantalla de Countrle en un iPhone"></img>
+              </div>
+            </div>
 
-            ¡Diviértete mientras aprendes y desafías a tus amigos! ¿Serás capaz de adivinar todas las palabras relacionadas con los países correctamente?
+            <hr />
 
-            ¡Que comience la aventura en Countrle!.</p>
-            <a className="btn btn-large btn-primary" href="/login">Iniciar Sesión</a>
+            <div class="row featurette">
+              <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Aprende mientras te diviertes <span class="text-muted">y reta a tus amigos.</span></h2>
+                <p class="lead">
+                  Countrle es más que un simple juego; es una herramienta educativa que te ayudará a mejorar tus habilidades lingüísticas y de resolución de problemas, a la vez que te ofrece la oportunidad de sumergirte en la rica diversidad cultural de nuestro planeta.
+
+                  Desafía a tus amigos, sube de nivel y compite en la clasificación global para convertirte en el mayor experto en geografía de Countrle. ¡Sé parte de la comunidad Countrle y comienza a descubrir el mundo de una forma nueva y emocionante hoy mismo!"
+                </p>
+              </div>
+              <div class="col-md-5 order-md-1">
+                <img class="featurette-image img-fluid mx-auto" src={trofeo} alt="Trofeo y birrete"></img>
+              </div>
+            </div>
+
+            <hr />
+
+            <div class="row featurette">
+              <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Juega a Countrle <span class="text-muted">desde cualquier lugar.</span></h2>
+                <p class="lead">
+                  Como aplicación progresiva, Countrle ofrece la máxima eficiencia y accesibilidad. Puedes instalarla directamente en tu dispositivo* y usarla como una aplicación nativa. Esto significa que puedes jugar Countrle sin importar si tienes conexión a internet o no, y experimentarás una velocidad y rendimiento óptimos en todo momento. Además, recibirás las actualizaciones tan pronto como estén disponibles, asegurando que siempre tengas la última versión de Countrle.
+                  <br /><br />
+                </p>
+              </div>
+              <div class="col-md-5 order-md-1">
+                <img class="featurette-image img-fluid mx-auto" src={captura_pwa} alt="Captura de Countrle instalada en un iPhone"></img>
+              </div>
+            </div>
+            <br /><br />
+            <p class="aviso">*Las PWA requieren de un dispositivo iOS 11.3, Android 5.0 (Lollipop) o superior y Chrome, Firefox, Safari y Edge actualizados para sistemas de escritorio. </p>
+
+            <hr />
+
+          </div>
 
         </div>
-
       </div>
-      
     </div>
   );
 };
-
 export default Inicio;
