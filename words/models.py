7e8205096343c4700e831c433ebdca3c76ee9c3b
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-01 21:30:17 +0200

    Modified the model for ObjectId

--- words/models.py
+++ words/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
+    _id = models.ObjectIdField()
     Name = models.CharField(default='nulo', max_length=200)
     Flag = models.CharField(default='nulo', max_length=4000)
 
 class Word(models.Model):
+    _id = models.ObjectIdField()
     Word = models.CharField(max_length=5)
-    Country = models.ForeignKey(Country, default=1, on_delete=models.CASCADE)
+    Country = models.ObjectIdField()
     Clue = models.CharField(max_length=400)
 
 
