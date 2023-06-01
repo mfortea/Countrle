ec26ccfc6b03ae86e5dea339dada24a856fdfe0e
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-01 21:19:31 +0200

    Modifying serializers another

--- words/serializers.py
+++ words/serializers.py
@@ -4,11 +4,11 @@ from .models import Word, Country
 class WordSerializer(ModelSerializer):
     class Meta:
         model = Word
-        fields = ('id', 'Word','Country', 'Clue')
-        read_only_fields = ('id',)
+        fields = ('_id', 'Word','Country', 'Clue')
+        read_only_fields = ('_id',)
 
 class CountrySerializer(ModelSerializer):
     class Meta:
         model = Country
-        fields = ('id', 'Name', 'Flag')
-        read_only_fields = ('id',) 
+        fields = ('_id', 'Name', 'Flag')
+        read_only_fields = ('_id',) 
