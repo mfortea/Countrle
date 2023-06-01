b9d64f1218459261b6a324ab940cdd905883a756
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-01 21:09:01 +0200

    Modified the model for ANTONIO

--- words/models.py
+++ words/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
-    name = models.CharField(default='nulo', max_length=200)
-    flag = models.CharField(default='nulo', max_length=4000)
+    Name = models.CharField(default='nulo', max_length=200)
+    Flag = models.CharField(default='nulo', max_length=4000)
 
 class Word(models.Model):
-    word = models.CharField(max_length=5)
-    country = models.ForeignKey(Country, default=1, on_delete=models.CASCADE)
-    clue = models.CharField(max_length=400)
+    Word = models.CharField(max_length=5)
+    Country = models.ForeignKey(Country, default=1, on_delete=models.CASCADE)
+    Clue = models.CharField(max_length=400)
 
 
     
--- words/serializers.py
+++ words/serializers.py
@@ -4,11 +4,11 @@ from .models import Word, Country
 class WordSerializer(ModelSerializer):
     class Meta:
         model = Word
-        fields = ('id', 'word','country', 'clue')
+        fields = ('_id', 'Word','Country', 'Clue')
         read_only_fields = ('id',)
 
 class CountrySerializer(ModelSerializer):
     class Meta:
         model = Country
-        fields = ('id', 'name', 'flag')
-        read_only_fields = ('id',) 
+        fields = ('_id', 'Name', 'Flag')
+        read_only_fields = ('_id',) 
