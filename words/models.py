240ea6712303c222b34c4defa9c899903ff4c23b
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-01 21:36:46 +0200

    Modifying the field of the id

--- words/models.py
+++ words/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Country(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.BinaryField()
     Name = models.CharField(default='nulo', max_length=200)
     Flag = models.CharField(default='nulo', max_length=4000)
 
 class Word(models.Model):
-    _id = models.ObjectIdField()
+    _id = models.BinaryField()
     Word = models.CharField(max_length=5)
-    Country = models.ObjectIdField()
+    Country = models.BinaryField()
     Clue = models.CharField(max_length=400)
 
 
