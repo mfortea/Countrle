80dd35ff66394d66b3c6bc401c8542d16c9cf712
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-02 13:41:43 +0200

    Added random words (time stamp)

--- words/views.py
+++ words/views.py
@@ -3,32 +3,28 @@ from .models import Word, Country
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
 import random
-from random import choice
 import datetime
 from .serializers import WordSerializer, CountrySerializer
-from bson.objectid import ObjectId
 
 # Create your views here.
 class allWord(viewsets.ModelViewSet):
-    queryset = Word.objects.values_list('_id', flat=True).order_by('_id')
-    randmon_id = choice(queryset)
-    queryset = Word.objects.filter(_id=ObjectId(randmon_id))
+    queryset = Word.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = WordSerializer
 
 class getWords(viewsets.ModelViewSet):
     queryset = None
     created = None
-    if(queryset == None or created != datetime.date.today()):
+    if (queryset == None or created != datetime.date.today()):
         random_idx = random.randint(0, Word.objects.count() - 1)
-        #queryset = Word.objects.filter(_id= pick_random_object())
+        queryset = Word.objects.filter(id=random_idx)
         created = datetime.date.today()
     permission_classes = [permissions.AllowAny]
     serializer_class = WordSerializer
 
 class getRandomWord(viewsets.ModelViewSet):
     random_idx = random.randint(0, Word.objects.count() - 1)
-    queryset = Word.objects.filter(_id=random_idx)
+    queryset = Word.objects.filter(id=random_idx)
     permission_classes = [permissions.AllowAny]
     serializer_class = WordSerializer
 
