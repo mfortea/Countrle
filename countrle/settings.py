69beca93ac7a381a877247b5b10160784ce2fee6
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-02 17:49:20 +0200

    Modified allowed ips and CORS

--- countrle/settings.py
+++ countrle/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default = 'django-insecure-86elhh!%!k-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['0.0.0.0', ]
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 
