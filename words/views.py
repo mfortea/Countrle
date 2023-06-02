3bd5c968dee321fbe1d3453033f00450e87ee75b
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-02 10:08:32 +0200

    Adding the last update from backend

--- words/views.py
+++ words/views.py
@@ -3,12 +3,16 @@ from .models import Word, Country
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
 import random
+from random import choice
 import datetime
 from .serializers import WordSerializer, CountrySerializer
+from bson.objectid import ObjectId
 
 # Create your views here.
 class allWord(viewsets.ModelViewSet):
-    queryset = Word.objects.all()
+    queryset = Word.objects.values_list('_id', flat=True).order_by('_id')
+    randmon_id = choice(queryset)
+    queryset = Word.objects.filter(_id=ObjectId(randmon_id))
     permission_classes = [permissions.AllowAny]
     serializer_class = WordSerializer
 
@@ -17,7 +21,7 @@ class getWords(viewsets.ModelViewSet):
     created = None
     if(queryset == None or created != datetime.date.today()):
         random_idx = random.randint(0, Word.objects.count() - 1)
-        queryset = Word.objects.all.get(random_idx)
+        #queryset = Word.objects.filter(_id= pick_random_object())
         created = datetime.date.today()
     permission_classes = [permissions.AllowAny]
     serializer_class = WordSerializer
