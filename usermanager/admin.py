5592e30712760eda6e76e6bcea33ec88ffc5963a
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-06-03 06:43:49 +0200

    Adding confs to administration panel

--- usermanager/admin.py
+++ usermanager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from usermanager.models import Statics
 
 # Register your models here.
+class StaticsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'score', 'time', 'totalWords')
+
+admin.site.register(Statics, StaticsAdmin)
+
--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from words.models import Country, Word
 
 # Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Name', 'Flag')
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Word', 'Country', 'Clue')
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Word, WordAdmin)
